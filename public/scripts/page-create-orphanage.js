1dd8ac7aa1c271cf3ba219d9560473208ea3f34d
Dara Maria <darasousa09@gmail.com>
2020-10-15 01:43:09 -0300

    scripts create-orphanage
    container de adicionar fotos está finalizado

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -29,3 +29,46 @@ map.on('click', (event) => {
     marker = L.marker([lat, lng], {icon})
     .addTo(map)
 })
+
+//adicionar o campo de fotos
+function addPhotoField(){
+    console.log("está adicionando")
+
+    //pegar o container de fotos #images
+    const container = document.querySelector('#images')
+
+    //pegar o container para duplicar a .new-image
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    //realizar o clone da ultima imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    //verificar se o campo estpa vazio, se sim não adicionar ao container de images
+    const input = newFieldContainer.children[0]
+
+    if(input.value == ""){
+        return
+    }
+    //limpar o campo antes de adicionar ao container de imagens
+    input.value = ""
+
+    //adicionar o clone ao container de #image
+    container.appendChild(newFieldContainer)
+}
+
+//remover container
+
+function deleteField(event) {
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length < 2){
+        //limpar o valor do campo
+        span.parentNode.children[0].value = ""
+        return
+    }
+
+    //deletar o campo
+    span.parentNode.remove();
+}
