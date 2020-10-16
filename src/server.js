754cd3103d07b757ed9c39dca6c799b37ba320f2
Dara Maria <darasousa09@gmail.com>
2020-10-15 22:45:29 -0300

    full
    front-end acoplado no back-end

---
+++ src/server.js
@@ -0,0 +1,28 @@
+//ctrl + L - limpar o terminal
+// node nome do servidor - para executar o servidorcd 
+//cd nome do diretorio - comando para entrar em um diretorio
+// cd ../ - voltar a pasta de origem
+
+//importar dependencias
+// const { response } = require('express')
+const express = require('express')
+const path = require('path')
+const pages = require('./pages.js')
+
+//inciando o express
+const server = express()
+server
+.use(express.static('public'))
+
+//configurar template engine
+.set('views', path.join(__dirname,'views'))
+.set('view engine', 'hbs')
+
+//rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+//ligar o servidor
+server.listen(5500)
